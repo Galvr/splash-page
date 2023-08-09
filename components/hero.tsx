bdd97b9505ad6f4998458ea1acf190bbee4fa1d0
Yi Sun <timyisun@gmail.com>
2023-08-08 20:27:40 -0500

    Updated background color and favicon.

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,8 +29,8 @@ export default function Hero() {
                 y2="577.921"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stopColor="#5D5DFF" stopOpacity=".01" />
-                <stop offset="1" stopColor="#5D5DFF" stopOpacity=".32" />
+                <stop stopColor="#7AA5EB" stopOpacity=".01" />
+                <stop offset="1" stopColor="#7AA5EB" stopOpacity=".32" />
               </linearGradient>
             </defs>
             <path
